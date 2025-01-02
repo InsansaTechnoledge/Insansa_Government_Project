0c6f12a74cdd86cb92eb49fc15a2885c20e84bea
Jay Fanse <jayf29112003@gmail.com>
2025-01-02 12:29:36 +0530

    latest update section

---
+++ insansagov/src/Components/Updates/LatestUpdateCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LatestUpdateCard = () => {
+  return (
+    <>
+    <div className='border p-3 bg-purple-100 rounded-md border-purple-600 hover:cursor-pointer hover:bg-purple-200'>
+        <div className='font-semibold'>
+            Latest Update 1 with link - 1/1/2025
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default LatestUpdateCard
--- insansagov/src/Components/Updates/LatestUpdates.jsx
+++ insansagov/src/Components/Updates/LatestUpdates.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import LatestUpdateCard from './LatestUpdateCard'
 
 const LatestUpdates = () => {
+
   return (
     <>
-        <div className='flex justify-between'>
-            <div className='font-bold'>Latest Updates</div>
-            <button>view more</button>
+        <div className='flex justify-between mb-5'>
+            <div className='font-bold text-xl flex items-center'>Latest Updates</div>
+            <button className='bg-purple-800 px-5 py-2 rounded-md text-white hover:bg-purple-900'>view more</button>
+        </div>
+        <div className='space-y-5'>
+            <LatestUpdateCard/>
+            <LatestUpdateCard/>
+            <LatestUpdateCard/>
         </div>
     </>
   )
