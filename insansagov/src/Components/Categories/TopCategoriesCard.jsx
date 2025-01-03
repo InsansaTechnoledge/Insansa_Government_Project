cb9d2210733c28cdb0edfa8246f7b23b0e22a180
Jay Fanse <jayf29112003@gmail.com>
2025-01-03 16:09:45 +0530

    coategory navigation added

--- insansagov/src/Components/Categories/TopCategoriesCard.jsx
+++ insansagov/src/Components/Categories/TopCategoriesCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import bank from '../../assets/Landing/bank.png'
+import { useNavigate } from 'react-router-dom'
 
 const TopCategoriesCard = () => {
+  const navigate = useNavigate();
   return (
-    <div className='flex flex-col justify-center hover:cursor-pointer hover:scale-110 transition-all duration-300'>
+    <div 
+    onClick={()=>navigate('/category')}
+    className='flex flex-col justify-center hover:cursor-pointer hover:scale-110 transition-all duration-300'>
         <div className='flex justify-center'>
             <img
             className='w-24'
