d8b60f5c5a3c41bb4302db3e0ec202e0cc5ee28f
Jay Fanse <jayf29112003@gmail.com>
2025-01-09 14:39:43 +0530

    added in routes

--- Server/routes/organisationRoutes.js
+++ Server/routes/organisationRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { getLogos } from "../controller/organisationController";
 
 
 const router = express.Router();
 
-router.get('/logo');
+router.get('/logo', getLogos);
+
+
+export default router;
 
--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -1,4 +1,8 @@
+import organisationRoute from './organisationRoutes.js'
+
 const routes=(app)=>{
-   
+
+    app.use('/api/organisation', organisationRoute);
+    
 };
 export default routes;
